c0f64ca8c7d69005b2dce4158fd08ea260f0cbcc
Sarthak Naithani <sarthaknaithani127@gmail.com>
2023-06-19 14:51:05 +0530

    made soem changes

--- extension.js
+++ extension.js
@@ -2,15 +2,93 @@ const vscode = require('vscode');
 
 function activate(context) {
 
+	const numOfMultiImports = 2;
+
+	const importsParser = (params) => {
+		let { multiImportsArray = [], singleImportsArray = [] } = params
+
+		let multiImportsString = "";
+
+		for (let i = 0; i < multiImportsArray?.length; i++) {
+			let str = multiImportsArray?.[i] ?? "";
+
+			/**
+			 * import comp1, comp2, { a1, a2, a3, a4 } from 'library' 
+			 * 
+			 * import -> import
+			 * whitespace -> \s*
+			 * comp1, comp2, ... ,compn -> ([^}]*)
+			 * whitespace -> \s*
+			 * { a1, a2, a3, a4 } -> {\s*([^}]*)}
+			 * whitespace -> \s*
+			 * from -> from
+			 * " or ' -> ["']
+			 * library -> ([^"']+)
+			 * " or ' -> ["']
+			 */
+			const libraryAndComponentsRegex = /import\s*([^}]*){\s*([^}]*)}\s*from\s*["']([^"']+)["']/;
+			const match = str.match(libraryAndComponentsRegex);
+
+			if (match) {
+				const [, defaultImports, components, libraryName] = match;
+				const componentList = components.split(/\s*,\s*/);
+
+				if (componentList?.length >= numOfMultiImports) {
+					let defaultImportsStr = '';
+					defaultImports?.split(',')?.map((val) => {
+						let s = val?.trim();
+						if (s?.length) {
+							defaultImportsStr += `${s},`
+						}
+					})
+
+					let mainStr = `import `;
+					if (defaultImportsStr?.trim()?.length) {
+						mainStr += `${defaultImportsStr?.trim()} `
+					}
+					mainStr += `{`
+
+					const importLines = [
+						`${mainStr}`,
+						...componentList?.map(component => `    ${component?.trim()},`),
+						`} from "${libraryName}";`
+					];
+
+					const multilineImport = importLines.join('\n');
+
+					multiImportsString += `${multilineImport}\n\n`;
+				} else {
+					singleImportsArray?.push(str);
+				}
+			} else {
+				singleImportsArray?.push(str);
+			}
+		}
+
+		return multiImportsString;
+	}
+
+	const importsSortFunc = (a, b) => {
+		if (a?.length > b?.length) {
+			return 1;
+		}
+
+		if (a?.length === b?.length) {
+			return 0;
+		}
+
+		if (a?.length < b?.length) {
+			return -1;
+		}
+	}
+
 	const sortFunc = () => {
 		let editor = vscode.window.activeTextEditor
-		let editorText = editor?.document.getText();
 
 		let numOfLines = editor?.document?.lineCount ?? 0;
 
 		const importRegexp = /import/gm;
 		const multipleImportRegexp = /{/gm;
-
 		const dotRegexp = /\./gm;
 
 		let libraryImports = [];
@@ -21,6 +99,9 @@ function activate(context) {
 
 		let maxImportLineNum = 0;
 
+		/**
+		 * get library & components imports array
+		 */
 		for (let i = 0; i < numOfLines; i++) {
 			let lineAtPos = editor?.document?.lineAt(i);
 
@@ -53,85 +134,21 @@ function activate(context) {
 			}
 		}
 
-		const sortFunc = (a, b) => {
-			if (a?.length > b?.length) {
-				return 1;
-			}
-
-			if (a?.length === b?.length) {
-				return 0;
-			}
-
-			if (a?.length < b?.length) {
-				return -1;
-			}
-		}
-
-		let libraryMultiImportsString = "";
-		let componentMultiImortsString = "";
-
 		// Library Imports - parse multiImports & convert to multi line string
-		for (let i = 0; i < libraryMultiImports?.length; i++) {
-			let str = libraryMultiImports?.[i] ?? "";
-
-			const libraryAndComponentsRegex = /import\s*{\s*([^}]*)}\s*from\s*["']([^"']+)["']/;
-			const match = str.match(libraryAndComponentsRegex);
-
-			if (match) {
-				const [, components, libraryName] = match;
-				const componentList = components.split(/\s*,\s*/);
-
-				if (componentList?.length >= 5) {
-					const importLines = [
-						`import {`,
-						...componentList?.map(component => `    ${component},`),
-						`} from "${libraryName}";`
-					];
-
-					const multilineImport = importLines.join('\n');
-
-					libraryMultiImportsString += `${multilineImport}\n\n`;
-				} else {
-					libraryImports?.push(str);
-				}
-			} else {
-				libraryImports?.push(str);
-			}
-		}
+		let libraryMultiImportsString = importsParser({
+			singleImportsArray: libraryImports,
+			multiImportsArray: libraryMultiImports,
+		})
 
 		// Components Imports
-		for (let i = 0; i < componentsMultiImport?.length; i++) {
-			let str = componentsMultiImport?.[i] ?? "";
-
-			const libraryAndComponentsRegex = /import\s*{\s*([^}]*)}\s*from\s*["']([^"']+)["']/;
-			const match = str.match(libraryAndComponentsRegex);
-
-			if (match) {
-				const [, components, libraryName] = match;
-				const componentList = components.split(/\s*,\s*/);
-
-				if (componentList?.length >= 5) {
-					const importLines = [
-						`import {`,
-						...componentList?.map(component => `    ${component},`),
-						`} from "${libraryName}";`
-					];
-
-					const multilineImport = importLines.join('\n');
-
-					componentMultiImortsString += `${multilineImport}\n\n`;
-				} else {
-					libraryImports?.push(str);
-				}
-			} else {
-				libraryImports?.push(str);
-			}
-		}
+		let componentMultiImortsString = importsParser({
+			singleImportsArray: componentImports,
+			multiImportsArray: componentsMultiImport,
+		})
 
 		// sort imports array by length
-		let sortedComponentsArray = componentImports?.sort(sortFunc)
-
-		let sortedLibraryArray = libraryImports?.sort(sortFunc)
+		let sortedComponentsArray = componentImports?.sort(importsSortFunc)
+		let sortedLibraryArray = libraryImports?.sort(importsSortFunc)
 
 		let sortedText = ``;
 		sortedLibraryArray?.map((text) => {
